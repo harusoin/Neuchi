5c5086125d2a1a4ed15521a5b5a46fb1cb382120
harusoin <harusoin@gmail.com>
2018-10-13 00:05:23 +0900

    feat: remake NusonController

--- Neuchi/nusoncontroller.cpp
+++ Neuchi/nusoncontroller.cpp
@@ -1,13 +1,12 @@
 #include "nusoncontroller.h"
-#include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/json_parser.hpp>
 
 using namespace boost::property_tree;
 using namespace neuchi::nuson;
 
 template<typename T>
-T PtreeToNusonData(const ptree &nusontree, std::string accessor,
-	boost::optional<T> initial = boost::none) {
+T PtreeToNusonData(const ptree &nusontree, const std::string &accessor,
+	const boost::optional<T> initial = boost::none) {
 
 	if (boost::optional<T> value = nusontree.get_optional<T>(accessor)) {
 		return value.get();
@@ -16,60 +15,136 @@ T PtreeToNusonData(const ptree &nusontree, std::string accessor,
 		if (initial) {
 			return initial.get();
 		}
-		throw "undefined " + accessor;
+		throw std::runtime_error("undefined " + accessor);
 	}
 }
 
-void NusonController::LoadNusonFile(const std::string &filename)
+bool NusonController::CheckSyntax()
 {
-	std::unique_ptr<NusonData> temp_nuson_data(new NusonData);
-	ptree current_nuson_raw_data;
-	read_json(filename, current_nuson_raw_data);
+	if (boost::optional<std::string> value = current_nuson_data.get_optional<std::string>("version")) {
+		std::string y = value.get();
+		return std::any_of(kSupportedNusonVersion.begin(), kSupportedNusonVersion.end(), [&y](std::string x) { return x == y; });
+	}
+	return false;
+}
 
+void NusonController::ReadNusonFile(const std::string &filepath)
+{
 	try
 	{
-		temp_nuson_data->version = PtreeToNusonData<double>(current_nuson_raw_data, "version");
-		temp_nuson_data->info.mode_hint = PtreeToNusonData<std::string>(current_nuson_raw_data, "info.mode_hint");
-		temp_nuson_data->info.title = PtreeToNusonData<std::string>(current_nuson_raw_data, "info.title");
-		temp_nuson_data->info.artist = PtreeToNusonData<std::string>(current_nuson_raw_data, "info.artist");
-		temp_nuson_data->info.notes_designer = PtreeToNusonData<std::string>(current_nuson_raw_data, "info.notes_designer");
-		temp_nuson_data->info.level = PtreeToNusonData<std::string>(current_nuson_raw_data, "info.level");
-		temp_nuson_data->info.genre = PtreeToNusonData<std::string>(current_nuson_raw_data, "info.genre");
-		temp_nuson_data->info.chart_name = PtreeToNusonData<std::string>(current_nuson_raw_data, "info.chart_name");
-		temp_nuson_data->info.init_bpm = PtreeToNusonData<double>(current_nuson_raw_data, "info.init_bpm");
-		temp_nuson_data->info.back_image = PtreeToNusonData<std::string>(current_nuson_raw_data, "info.back_image", boost::optional<std::string>(""));
-		temp_nuson_data->info.banner_image = PtreeToNusonData<std::string>(current_nuson_raw_data, "info.banner_image", boost::optional<std::string>(""));
-		temp_nuson_data->info.preview_music = PtreeToNusonData<std::string>(current_nuson_raw_data, "info.preview_music", boost::optional<std::string>(""));
-		temp_nuson_data->info.resolution = PtreeToNusonData<int>(current_nuson_raw_data, "info.resolution");
-		temp_nuson_data->info.width_resolution = PtreeToNusonData<int>(current_nuson_raw_data, "info.width_resolution");
-		temp_nuson_data->info.copyright = PtreeToNusonData<std::string>(current_nuson_raw_data, "info.copyright", boost::optional<std::string>(""));
-		temp_nuson_data->enemy.name = PtreeToNusonData<std::string>(current_nuson_raw_data, "enemy.name");
-		temp_nuson_data->enemy.level = PtreeToNusonData<int>(current_nuson_raw_data, "enemy.level");
-		temp_nuson_data->enemy.attribute = PtreeToNusonData<std::string>(current_nuson_raw_data, "enemy.attribute");
-		temp_nuson_data->enemy.hit_point = PtreeToNusonData<int>(current_nuson_raw_data, "enemy.hit_point");
-		temp_nuson_data->objects = new std::vector<NusonObjectData>;
-		for (auto child : current_nuson_raw_data.get_child("objects")) {
-			NusonObjectData t;
-			const ptree& objects = child.second;
-			t.type = PtreeToNusonData<std::string>(objects, "type");
-			t.notes = new std::vector<NusonNoteData>;
-			for (auto child_object : objects.get_child("notes")) {
-				NusonNoteData t2;
-				const ptree& notes = child_object.second;
-				t2.type = PtreeToNusonData<std::string>(notes, "type");
-				t2.x = PtreeToNusonData<int>(notes, "x", 0);
-				t2.y = PtreeToNusonData<int>(notes, "y", 0);
-				t2.l = PtreeToNusonData<int>(notes, "l", 0);
-				t.notes->push_back(t2);
-			}
-			temp_nuson_data->objects->push_back(t);
-		}
+		read_json(filepath, current_nuson_data);
+		if (!CheckSyntax()) throw std::runtime_error("NUSON file syntax error");
 	}
-	catch (std::string e)
+	catch (boost::property_tree::json_parser_error& e)
 	{
 		throw;
 	}
-	current_nuson_data = std::move(temp_nuson_data);
+	catch (std::runtime_error& e)
+	{
+		throw;
+	}
+}
+
+bool NusonController::ReadNusonFile()
+{
+	if (current_nuson_file_path == boost::none) return false;
+	try
+	{
+		ReadNusonFile(current_nuson_file_path.get());
+	}
+	catch (boost::property_tree::json_parser_error& e)
+	{
+		throw;
+	}
+	return true;
+}
+
+void NusonController::WriteNusonFile(const std::string & filepath)
+{
+	try
+	{
+		write_json(filepath, current_nuson_data);
+	}
+	catch (boost::property_tree::json_parser_error& e)
+	{
+		throw;
+	}
+}
+
+bool NusonController::WriteNusonFile()
+{
+	if (current_nuson_file_path == boost::none) return false;
+	try
+	{
+		WriteNusonFile(current_nuson_file_path.get());
+	}
+	catch (boost::property_tree::json_parser_error& e)
+	{
+		throw;
+	}
+	return true;
+}
+
+std::string NusonController::GetNusonVersion()
+{
+	return current_nuson_version;
+}
+
+NusonInfoData NusonController::GetNusonInfoData()
+{
+	NusonInfoData d;
+	try
+	{
+		d.mode_hint = PtreeToNusonData <std::string>(current_nuson_data, "info.mode_hint", std::string(""));
+		d.title = PtreeToNusonData <std::string>(current_nuson_data, "info.title", std::string(""));
+		d.artist = PtreeToNusonData <std::string>(current_nuson_data, "info.artist", std::string(""));
+		d.notes_designer = PtreeToNusonData <std::string>(current_nuson_data, "info.notes_designer", std::string(""));
+		d.level = PtreeToNusonData <std::string>(current_nuson_data, "info.level", std::string(""));
+		d.genre = PtreeToNusonData <std::string>(current_nuson_data, "info.genre", std::string(""));
+		d.chart_name = PtreeToNusonData <std::string>(current_nuson_data, "info.chart_name", std::string(""));
+		d.init_bpm = PtreeToNusonData <double>(current_nuson_data, "info.init_bpm", 0);
+		d.back_image = PtreeToNusonData <std::string>(current_nuson_data, "info.back_image", std::string(""));
+		d.banner_image = PtreeToNusonData <std::string>(current_nuson_data, "info.banner_image", std::string(""));
+		d.preview_music = PtreeToNusonData <std::string>(current_nuson_data, "info.preview_music", std::string(""));
+		d.resolution = PtreeToNusonData <int>(current_nuson_data, "info.resolution", kDefaultResolution);
+		d.width_resolution = PtreeToNusonData <int>(current_nuson_data, "info.width_resolution", kDefaultWidthResolution);
+		d.copyright = PtreeToNusonData <std::string>(current_nuson_data, "info.copyright", std::string(""));
+	}
+	catch (boost::property_tree::json_parser_error& e)
+	{
+		throw;
+	}
+	return d;
+}
+
+NusonEnemyData NusonController::GetNusonEnemyData()
+{
+	NusonEnemyData d;
+	try
+	{
+		d.name = PtreeToNusonData <std::string>(current_nuson_data, "enemy.name", std::string(""));
+		d.level = PtreeToNusonData <int>(current_nuson_data, "enemy.level", 0);
+		d.attribute = PtreeToNusonData <std::string>(current_nuson_data, "enemy.attribute", std::string(""));
+		d.hit_point = PtreeToNusonData <int>(current_nuson_data, "enemy.hit_point", 0);
+	}
+	catch (const std::exception&)
+	{
+		throw;
+	}
+	return d;
+}
+
+NusonController::NusonController(const std::string &filepath)
+{
+	current_nuson_file_path = filepath;
+	try
+	{
+		ReadNusonFile(filepath);
+	}
+	catch (boost::property_tree::json_parser_error& e)
+	{
+		throw;
+	}
 }
 
 NusonController::~NusonController()
--- Neuchi/nusoncontroller.h
+++ Neuchi/nusoncontroller.h
@@ -8,14 +8,17 @@
 #define NEUCHI_NUSONCONTROLLER_H
 
 #include <string>
-#include <memory>
+#include <optional>
 #include <vector>
-
+#include <boost/property_tree/ptree.hpp>
 
 namespace neuchi
 {
 namespace nuson
 {
+const std::vector<std::string> kSupportedNusonVersion = {"0.1"};
+const int kDefaultResolution = 240;
+const int kDefaultWidthResolution = 120;
 
 struct NusonNoteData
 {
@@ -29,58 +32,52 @@ struct NusonObjectData
 	std::string type;
 	std::vector<NusonNoteData>* notes;
 };
-struct NusonData
+struct NusonEnemyData
 {
-	double version;
-	struct
-	{
-		std::string mode_hint;
-		std::string title;
-		std::string artist;
-		std::string notes_designer;
-		std::string level;
-		std::string genre;
-		std::string chart_name;
-		double init_bpm;
-		std::string back_image;
-		std::string banner_image;
-		std::string preview_music;
-		int resolution;
-		int width_resolution;
-		std::string copyright;
-
-	}info;
-	struct
-	{
-		std::string name;
-		int level;
-		std::string attribute;
-		int hit_point;
-	}enemy;
-	std::vector<NusonObjectData>* objects;
-	NusonData() : objects(nullptr) {};
-	~NusonData() {
-		if (objects != nullptr)
-		{
-			for (auto o : *objects) {
-				delete o.notes;
-			}
-		}
-	}
-private:
-	void operator =(const NusonData& src) {}
-	NusonData(const NusonData& src) {}
+	std::string name;
+	int level;
+	std::string attribute;
+	int hit_point;
+};
+struct NusonInfoData
+{
+	std::string mode_hint;
+	std::string title;
+	std::string artist;
+	std::string notes_designer;
+	std::string level;
+	std::string genre;
+	std::string chart_name;
+	double init_bpm;
+	std::string back_image;
+	std::string banner_image;
+	std::string preview_music;
+	int resolution;
+	int width_resolution;
+	std::string copyright;
 };
 
 class NusonController
 {
 public:
-	NusonController() {}
+	NusonController() : current_nuson_file_path(boost::none)
+					, current_nuson_version(kSupportedNusonVersion.back()){}
+	NusonController(const std::string &filepath);
 	NusonController(const NusonController &from) {}
-	void LoadNusonFile(const std::string &filename);
 	~NusonController();
+
+	bool CheckSyntax();
+	void ReadNusonFile(const std::string &filepath);
+	bool ReadNusonFile();
+	void WriteNusonFile(const std::string &filepath);
+	bool WriteNusonFile();
+	std::string GetNusonVersion();
+	NusonInfoData GetNusonInfoData();
+	NusonEnemyData GetNusonEnemyData();
 private:
-	std::unique_ptr<NusonData> current_nuson_data;
+	boost::optional<std::string> current_nuson_file_path;
+	boost::property_tree::ptree current_nuson_data;
+	std::string current_nuson_version;
 };
 }
 }
